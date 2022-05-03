012a5c3d173050a19943ee437b7f9cdd21d41b87
Key <jh.yield@gmail.com>
2022-05-03 16:45:20 +1000

    updated questions and lib js

--- index.js
+++ index.js
@@ -2,26 +2,24 @@
 //packages needed for the application
 const inquirer = require('inquirer');
 const fs = require('fs');
-//allows for looping in inquirer
-inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
 //modules referenced
 
-const questions = require("./lib/questions")
-const generateHTML = require("./lib/generateHTML")
+const questions = require('./lib/question2');
+
 
 
 const init = () => {
     console.log("This is the team profile generator.\n  \nAs a Manager, please enter the following.")
     questions()
-
-        .then((answers) => {
-
-            fs.writeFileSync('./dist/index.html', generateHTML(answers))
-        })
-        .then(() => console.log('Successfully finished questions'))
-
     // .catch((err) => console.error(err));
 };
 
 init();
+//create manager function, when its done
+//  another function to create team , this will ask one mc question intern or eng? or done
+//      if eng call eng function
+//      if intern, call intern
+//      if none then writes the files
+
+

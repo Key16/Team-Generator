33ad01814fc15728a606ba78350b1dabedb13aea
Key <jh.yield@gmail.com>
2022-05-05 01:43:27 +1000

    updated intro welcome message

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
-//modules referenced
-
 const questions = require('./lib/questions');
 
 
 
 const init = () => {
-    console.log("This is the team profile generator.\n  \nAs a Manager, please enter the following.")
+    console.log("Welcome! This is the team profile generator.\n  \nAs a Manager, please enter the following.")
     questions()
 };
 
